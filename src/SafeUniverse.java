5c834f81f0f173f35ffd803c7e81831319ec5144
tommikulevich <tommikulevich@gmail.com>
2022-06-06 21:03:39 +0200

    Rearranged cylinders in array. Added latch (pl. "zatrzask") when rotating cylinders. Small changes and code optimizations.

--- src/SafeUniverse.java
+++ src/SafeUniverse.java
@@ -23,7 +23,7 @@ public class SafeUniverse extends JPanel implements ActionListener, KeyListener
     private static final int PHEIGHT = 512;
     private static final int BOUNDSIZE = 100;   // larger than the world
 
-    private static final Point3d USERPOS = new Point3d(0,5,-20); // initial user position
+    private static final Point3d USERPOS = new Point3d(0,5,20); // initial user position
 
     private final SimpleUniverse su;
     private BranchGroup sceneBG;
@@ -38,12 +38,14 @@ public class SafeUniverse extends JPanel implements ActionListener, KeyListener
     private final Button setDefaultViewButton = new Button("Default View");
 
     private final ArrayList<TransformGroup>rotCyl = new ArrayList<>();
+    private boolean latchLeft = false;
+    private boolean latchRight = false;
 
     // parameters of cylinders
     int numOfCyl = 5;
     float disBetCyl = 1.0f;
     float vertPos = 3.0f;
-    float cylRad = 1.0f;
+    float cylRad = 1.5f;
     float cylH = 0.5f;
     float axRad = 0.1f;
     int posOfFirstCyl = 2;
@@ -185,12 +187,54 @@ public class SafeUniverse extends JPanel implements ActionListener, KeyListener
         // creating cylinders
         ArrayList<Cylinder>cylinders = new ArrayList<>();
 
-        // setting params of cylinders
-        for(int i = 0; i < numOfCyl; i++)
+        // setting params for main cylinder
+        cylinders.add(new Cylinder(cylRad*1.5f, cylH, Cylinder.GENERATE_NORMALS| Cylinder.GENERATE_TEXTURE_COORDS, 80, 80, woodStyle));
+
+        // creating lines on main cylinder
+        for (int i = 0; i < 5; i++) {
+            Appearance app = new Appearance();
+            app.setColoringAttributes(new ColoringAttributes(new Color3f(Color.BLACK), ColoringAttributes.NICEST));
+
+            Box line = new Box(axRad, cylH/1.9f, cylRad*1.2f, app);
+
+            Transform3D lineRot = new Transform3D();
+            lineRot.rotY(i*Math.PI/5);
+
+            TransformGroup lineTransform = new TransformGroup(lineRot);
+            lineTransform.addChild(line);
+
+            cylinders.get(0).addChild(lineTransform);
+        }
+
+        // creating numbers on main cylinder
+        for (int i = 0; i < 10; i++){
+            Text2D num = new Text2D(Integer.toString(i), new Color3f(Color.BLACK), "SansSerif", 100, Font.BOLD );
+
+            Transform3D numPos = new Transform3D();
+            numPos.set(new Vector3f(-0.1f, cylRad*1.2f, cylH/1.9f));
+
+            Transform3D numRot1 = new Transform3D();
+            numRot1.rotX(-Math.PI/2);
+            numRot1.mul(numPos);
+
+            Transform3D numRot2 = new Transform3D();
+            numRot2.rotY(-i*Math.PI/5);
+
+            TransformGroup numTransform1 = new TransformGroup(numRot1);
+            numTransform1.addChild(num);
+
+            TransformGroup numTransform2 = new TransformGroup(numRot2);
+            numTransform2.addChild(numTransform1);
+
+            cylinders.get(0).addChild(numTransform2);
+        }
+
+        // setting params of other cylinders
+        for(int i = 1; i <= numOfCyl; i++)
             cylinders.add(new Cylinder(cylRad, cylH, Cylinder.GENERATE_NORMALS| Cylinder.GENERATE_TEXTURE_COORDS, 80, 80, woodStyle));
 
-        // creating lines on cylinders
-        for (int i = 0; i < numOfCyl; i++){
+        // creating lines on other cylinders
+        for (int i = 1; i <= numOfCyl; i++){
             Appearance app = new Appearance();
             app.setColoringAttributes(new ColoringAttributes(new Color3f(Color.BLACK), ColoringAttributes.NICEST));
 
@@ -210,74 +254,23 @@ public class SafeUniverse extends JPanel implements ActionListener, KeyListener
 
         //setting this transformation by position algorithm
         int posOfLastCyl = posOfFirstCyl;
-        for (int i = 0; i < numOfCyl; i++, posOfLastCyl--){
+        for (int i = 0; i <= numOfCyl; i++, posOfLastCyl--){
             Transform3D k = new Transform3D();
             k.set(new Vector3f(0, disBetCyl*posOfLastCyl, -vertPos));
             posCyl.add(k);
         }
 
         // setting params for axis by axis algorithm
-        cylinders.add(new Cylinder(axRad, disBetCyl*(posOfFirstCyl-posOfLastCyl), Cylinder.GENERATE_NORMALS| Cylinder.GENERATE_TEXTURE_COORDS, 80, 80, woodStyle));
+        cylinders.add(new Cylinder(axRad, disBetCyl*(posOfFirstCyl-posOfLastCyl-1), Cylinder.GENERATE_NORMALS| Cylinder.GENERATE_TEXTURE_COORDS, 80, 80, woodStyle));
 
         // creating and setting position transformation for axis
         Transform3D p = new Transform3D();
-        p.set(new Vector3f(0, disBetCyl*(posOfFirstCyl+posOfLastCyl)/2,-vertPos));
+        p.set(new Vector3f(0, disBetCyl*(posOfFirstCyl+posOfLastCyl+1)/2,-vertPos));
         posCyl.add(p);
-
-        // setting params for main cylinder
-        cylinders.add(new Cylinder(cylRad*2.0f, cylH, Cylinder.GENERATE_NORMALS| Cylinder.GENERATE_TEXTURE_COORDS, 80, 80, woodStyle));
-
-        // creating and setting position transformation for main cylinder
-        Transform3D u = new Transform3D();
-        u.set(new Vector3f(0, disBetCyl*posOfLastCyl, -vertPos));
-        posCyl.add(u);
-
-        // creating lines on main cylinder
-        for (int i = 0; i < 5; i++) {
-            Appearance app = new Appearance();
-            app.setColoringAttributes(new ColoringAttributes(new Color3f(Color.BLACK), ColoringAttributes.NICEST));
-
-            Box line = new Box(axRad, cylH/1.9f, cylRad*1.6f, app);
-
-            Transform3D lineRot = new Transform3D();
-            lineRot.rotY(i*Math.PI/5);
-
-            TransformGroup lineTransform = new TransformGroup(lineRot);
-            lineTransform.addChild(line);
-
-            cylinders.get(numOfCyl+1).addChild(lineTransform);
-        }
-
-        // creating numbers on main cylinder
-        for (int i = 0; i < 10; i++){
-            Text2D num = new Text2D(Integer.toString(i), new Color3f(Color.BLACK), "SansSerif", 100, Font.BOLD );
-
-            Transform3D numPos = new Transform3D();
-            numPos.set(new Vector3f(-0.1f, cylRad*1.6f, cylH/1.9f));
-
-            Transform3D numRot1 = new Transform3D();
-            numRot1.rotX(Math.PI/2);
-            numRot1.mul(numPos);
-
-            Transform3D numRot2 = new Transform3D();
-            numRot2.rotY(Math.PI);
-            numRot2.mul(numRot1);
-
-            Transform3D numRot3 = new Transform3D();
-            numRot3.rotY(i*Math.PI/5);
-
-            TransformGroup numTransform1 = new TransformGroup(numRot2);
-            numTransform1.addChild(num);
-
-            TransformGroup numTransform2 = new TransformGroup(numRot3);
-            numTransform2.addChild(numTransform1);
-
-            cylinders.get(numOfCyl+1).addChild(numTransform2);
-        }
 
         // creating transformation groups and matching with transformation
         ArrayList<TransformGroup>tg = new ArrayList<>();
-        for (int i = 0; i < numOfCyl+2; i++){
+        for (int i = 0; i <= numOfCyl+1; i++){
             TransformGroup k = new TransformGroup(posCyl.get(i));
             tg.add(k);
 
@@ -291,9 +284,8 @@ public class SafeUniverse extends JPanel implements ActionListener, KeyListener
         TransformGroup tg_rot = new TransformGroup(tmp_rot);
 
         // matching transformation groups with rotation
-        for (int i = 0; i < numOfCyl+2; i++){
+        for (int i = 0; i <= numOfCyl+1; i++){
             rotCyl.get(i).setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
-
             rotCyl.get(i).addChild(cylinders.get(i));
             tg.get(i).addChild(rotCyl.get(i));
             tg_rot.addChild(tg.get(i));
@@ -329,29 +321,36 @@ public class SafeUniverse extends JPanel implements ActionListener, KeyListener
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource() == startButton){
+        if(e.getSource() == startButton)
             if(!clock1.isRunning())
                 clock1.start();
-        }
-        else if(e.getSource() == stopButton){
+        else if(e.getSource() == stopButton)
             if(clock1.isRunning())
                 clock1.stop();
-        }
-        else if(leftButton) {
-            angle -= Math.PI / 5;
-        }
-        else if(rightButton) {
-            angle += Math.PI / 5;
-        }
-        else if(e.getSource() == setDefaultViewButton){
+
+        if(e.getSource() == setDefaultViewButton)
             initUserPosition();
+
+        if(leftButton)
+            latchLeft = true;
+        else if(latchLeft) {
+            angle += Math.PI/5;
+            latchLeft = false;
+        }
+        else if(rightButton)
+            latchRight = true;
+        else if(latchRight) {
+            angle -= Math.PI/5;
+            latchRight = false;
         }
 
-        Transform3D rot = new Transform3D();
-        rot.rotY(angle);
+        if(!latchLeft && !latchRight) {
+            Transform3D rot = new Transform3D();
+            rot.rotY(angle);
 
-        for (int i = 0; i < numOfCyl+2; i++)
-            rotCyl.get(i).setTransform(rot);
+            for (int i = 0; i <= numOfCyl+1; i++)
+                rotCyl.get(i).setTransform(rot);
+        }
 
     }
 
